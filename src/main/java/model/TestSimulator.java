dffc8c07fc8442232146dd52c6ae58e50d5bcd19
lorenzomelchionna <lmelchionna73@gmail.com>
2021-05-18 10:17:59 +0200

    Versione fatta a lezione

--- src/main/java/model/TestSimulator.java
+++ src/main/java/model/TestSimulator.java
@@ -3,7 +3,11 @@ package model;
 public class TestSimulator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Simulator sim = new Simulator();
+		
+		sim.init();
+		sim.run();
 
 	}
 
